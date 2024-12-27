7e53d80f6ab1d9ad13d3a158031ad85dd4683178
Harsh Mriduhash <Harshsahay2709@gmail.com>
2024-12-27 11:40:25 +0530

    re-doing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Brain className="h-8 w-8 text-indigo-600" />
-            <span className="ml-2 text-xl font-bold text-gray-900">DevAI Hub</span>
+            <span className="ml-2 text-xl font-bold text-gray-900">Lancer</span>
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
